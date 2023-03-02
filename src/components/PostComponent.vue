7a65e68c1b038e23db16f4939f3f3a37fef3adf5
TommasoBernardo <tommasob1210@gmail.com>
2023-03-02 20:52:17 +0100

    update

--- src/components/PostComponent.vue
+++ src/components/PostComponent.vue
@@ -12,13 +12,14 @@ export default {
 </script>
 
 <template lang="">
-    <div class="col-5 post-card mt-3 border" v-for="post in posts">                       
-        <img :src="post.image" class="card-img-top" alt="#">
-        <div class="card-body">
-            <h5 class="card-title">{{post.title}}</h5>
-            <p class="card-text">{{post.content.substr(0, 120)}}</p>
-            <a href="#" class="btn btn-primary">Show more</a>
+        <div class="col-6 single-post card mt-3" v-for="post in posts">
+            <img :src="post.image" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"> {{ post.title }}</h5>
+                    <h5 class="card-date"> {{ post.date }}</h5>
+                    <p class="card-text">{{ post.content.substr(0, 160) }}</p>
+                    <a href="#" class="btn btn-primary">show more</a>
+                </div>
         </div>
-    </div>
 </template>
 
