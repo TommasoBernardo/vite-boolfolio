0a70be84434f538a9c49a38a758854dfb3edd418
TommasoBernardo <tommasob1210@gmail.com>
2023-03-02 20:33:54 +0100

    finish exercise

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -37,7 +37,16 @@ export default {
         <div class="container">
             <div class="row">
                 <div class="col-12">
-                    <h2>ciao</h2>
+                    <h2>Films:</h2>
+                </div>
+                <div class="col-6 single-post card mt-3" v-for="post in posts">
+                        <img :src="post.image" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"> {{ post.title }}</h5>
+                            <h5 class="card-date"> {{ post.date }}</h5>
+                            <p class="card-text">{{ post.content.substr(0, 160) }}</p>
+                            <a href="#" class="btn btn-primary">show more</a>
+                        </div>
                 </div>
             </div>
         </div>
---
+++ src/components/PostComponent.vue
@@ -0,0 +1,24 @@
+
+<script>
+export default {
+    name: 'PostComponent',
+    data() {
+        return {
+        }
+    },
+    methods: {
+    },
+}
+</script>
+
+<template lang="">
+    <div class="col-5 post-card mt-3 border" v-for="post in posts">                       
+        <img :src="post.image" class="card-img-top" alt="#">
+        <div class="card-body">
+            <h5 class="card-title">{{post.title}}</h5>
+            <p class="card-text">{{post.content.substr(0, 120)}}</p>
+            <a href="#" class="btn btn-primary">Show more</a>
+        </div>
+    </div>
+</template>
+
