3f455658060817a0ec1be99fc41baa944f615099
TommasoBernardo <tommasob1210@gmail.com>
2023-03-03 17:57:26 +0100

    visualize posts

---
+++ src/components/AppPost.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+    name: 'AppPost',
+    data(){
+        return{
+
+        }
+    },
+    props:{
+        'post':{
+            type: Object,
+            required: true,
+        },
+    },
+}
+</script>
+
+<template lang="">
+    <div class="container">
+        <div class=" single-post card  w-100 p-3" >
+            <img :src="post.image" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title"> {{ post.title }}</h5>
+                <h5 class="card-date"> {{ post.date }}</h5>
+                <p class="card-text">{{ post.content.substr(0, 160) }}</p>
+                <router-link class="btn btn-primary">show more</router-link>
+            </div>
+    </div>
+    </div>
+
+</template>
+
+
+<style lang="">
+    
+</style>
--- src/pages/PostsIndex.vue
+++ src/pages/PostsIndex.vue
@@ -1,24 +1,51 @@
 
 <script>
+    import AppPost from '../components/AppPost.vue';
+    import axios from 'axios';
 export default {
     name: 'PostsIndex',
+    components: {
+        AppPost
+    },
     data(){
         return{
-
+            posts:[],
+            loading:false,
+            urlAddress: 'http://127.0.0.1:8000/api/posts',
         }
     },
-    mounted(){
-        console.warn(this.route)
+    
+    methods: {
+            getPosts() {
+                axios.get(this.urlAddress, {
+                    params:{
+
+                    }
+                })
+                .then((response)=>{
+                    this.posts = response.data.results.data;
+                })
+                .catch(function(error){
+                    console.warn(error);
+                });
+            }
+        },
+
+    created(){
+        this.getPosts();
     }
 }
 </script>
 
 <template lang="">
-    <div>
-        <h1>
-            hello
-        </h1>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <h2>Films:</h2>
+            </div>
+        </div>
     </div>
+    <AppPost v-for="post in posts" :post="post"/>
 </template>
 
 
