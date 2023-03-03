1fc0e0d5d46e8db4233ea496b476169142a8da56
TommasoBernardo <tommasob1210@gmail.com>
2023-03-03 19:28:07 +0100

    finish home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -6,9 +6,27 @@ export default {
 </script>
 
 <template lang="">
-    <section>
-        <div class="text-center mt-5">
+    <section class="HomePage size">
+        <div class="text-center ">
         <h1>Welcome in Films-boolfolio!</h1>
+        <p class="text-white">
+            Enjoy, all the news about movies of all kinds, provided by our site
+        </p>
+    </div>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-4 mt-5">
+                <img class="img-fluid" src="https://th.bing.com/th/id/R.d4c6f178b2b85c58bf674f0c0b0a1ebe?rik=ZMiG6d6Fm688XQ&pid=ImgRaw&r=0" alt="">
+            </div>
+            <div class="col-4 mt-5">
+                <img class="img-fluid"  src="https://th.bing.com/th/id/R.440ac2bb48e363910ba3e77d01cfd0ae?rik=%2fcBlK01vWq%2f1pA&pid=ImgRaw&r=0" alt="">
+            </div>
+
+            <div class="col-4 mt-5">
+                <img class="img-fluid"  src="https://th.bing.com/th/id/R.cec85d161c273dcc6d94debac2fe5a97?rik=LSqXVYhXMM6W2A&pid=ImgRaw&r=0" alt="">
+            </div>
+        </div>
     </div>
     </section>
 
